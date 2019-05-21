10f746b8bc2ea2f3e6ecf955eeba2de25d13c67d
m.ilnitsky <m.ilnitsky@academ-it.ru>
2019-05-21 20:25:18 +0700

    Task03_ArrayListHome: реализовано

---
+++ Task03_ArrayListHome/ArrayListHome.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace Task03_ArrayListHome
+{
+    class ArrayListHome
+    {
+        static void PrintList(ArrayList arrayList)
+        {
+            foreach (var element in arrayList)
+            {
+                Console.Write(element + ", ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("### 1: Прочитать в список строки из файла");
+            Console.WriteLine();
+
+            var listOfStrings = new ArrayList();
+
+            string fileName = "..\\..\\input.txt";
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    listOfStrings.Add(reader.ReadLine());
+                }
+            }
+
+            Console.WriteLine("listOfStrings from file '" + fileName + "':");
+            foreach (var str in listOfStrings)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("### 2: Удалить все чётные числа");
+            Console.WriteLine();
+
+            var listOfIntNumbers = new ArrayList() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+
+            Console.Write("listOfIntNumbers: ");
+            PrintList(listOfIntNumbers);
+            Console.WriteLine();
+
+            for (int i = listOfIntNumbers.Count - 1; i >= 0; --i)
+            {
+                if ((int)listOfIntNumbers[i] % 2 == 0)
+                {
+                    listOfIntNumbers.RemoveAt(i);
+                }
+            }
+
+            Console.Write("listOfIntNumbers: ");
+            PrintList(listOfIntNumbers);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("### 3: Создать новый список без повторяющихся чисел");
+            Console.WriteLine();
+
+            var listOfRandomNumbers = new ArrayList() { 1, 2, 3, 1, 2, 3, 4, 5, 6, 5, 6, 7, 8, 6, 7, 8, 9, 10, 8, 6, 4, 2, 1 };
+
+            Console.Write("listOfRandomNumbers: ");
+            PrintList(listOfRandomNumbers);
+            Console.WriteLine();
+
+            var listOfUniqueNumbers = new ArrayList();
+
+            foreach (var number in listOfRandomNumbers)
+            {
+                if (!listOfUniqueNumbers.Contains(number))
+                {
+                    listOfUniqueNumbers.Add(number);
+                }
+            }
+
+            Console.Write("listOfUniqueNumbers: ");
+            PrintList(listOfUniqueNumbers);
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Exit?");
+            Console.ReadLine();
+        }
+    }
+}
